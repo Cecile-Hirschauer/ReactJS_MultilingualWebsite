c585c0fe38da266d23600db306e883aabd3e8a6c
cecile.hirschauer <cecile.hirschauer@suricats-consulting.com>
2022-01-20 14:43:35 +0100

    Create context

--- src/Components/Contenu/Contenu.js
+++ src/Components/Contenu/Contenu.js
@@ -1,16 +1,20 @@
-import React, {useContext} from 'react';
-import './Contenu.css';
-import data from '../../assets/data.js';
+import React, { useContext } from "react";
+import { Context } from "../../context/langContext";
+import "./Contenu.css";
+import data from "../../assets/data.js";
 
 function Contenu() {
-  return (
-  <div>
-      <div className="content">
-          <h1 className="title">{data['FR'].title}</h1>
-          <p className="content-txt">{data['FR'].txt}</p>
-      </div>
-  </div>
-  );
+
+    const {lang} = useContext(Context);
+    // console.log(lang);
+    return (
+        <div>
+        <div className="content">
+            <h1 className="title">{data[lang].title}</h1>
+            <p className="content-txt">{data[lang].txt}</p>
+        </div>
+        </div>
+    );
 }
 
 export default Contenu;
